4d642d0b94ed84d70644a866db12807f6a97b17d
huchenhao <775656764@qq.com>
2020-10-30 02:43:02 +0800

    add code and mconfig api dep

--- cmd/mconfig.go
+++ cmd/mconfig.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	log "github.com/mhchlib/logger"
-	"github.com/mhchlib/mconfig/api"
+	"github.com/mhchlib/mconfig-api/api/sdk"
 	"github.com/mhchlib/mconfig/service"
 	"github.com/micro/go-micro/v2"
 )
@@ -15,7 +15,7 @@ func main() {
 	//log.SetReportCaller(true)
 	mService := micro.NewService()
 	mService.Init()
-	err := api.RegisterMConfigHandler(mService.Server(), &service.MConfig{})
+	err := sdk.RegisterMConfigHandler(mService.Server(), &service.MConfig{})
 	if err != nil {
 		log.Fatal(err)
 	}
