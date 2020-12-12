b0e50e9cd6c171c68471bc2931ca1860445fbb70
huchenhao <775656764@qq.com>
2020-12-12 17:43:56 +0800

    feature:add banner.go

--- cmd/mconfig.go
+++ cmd/mconfig.go
@@ -18,9 +18,20 @@ import (
 	"syscall"
 )
 
+/***
+ *        ███╗   ███╗ ██████╗ ██████╗ ███╗   ██╗███████╗██╗ ██████╗
+ *        ████╗ ████║██╔════╝██╔═══██╗████╗  ██║██╔════╝██║██╔════╝
+ *        ██╔████╔██║██║     ██║   ██║██╔██╗ ██║█████╗  ██║██║  ███╗
+ *        ██║╚██╔╝██║██║     ██║   ██║██║╚██╗██║██╔══╝  ██║██║   ██║
+ *        ██║ ╚═╝ ██║╚██████╗╚██████╔╝██║ ╚████║██║     ██║╚██████╔╝
+ *        ╚═╝     ╚═╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝     ╚═╝ ╚═════╝
+ *
+ */
+
 var mconfig *pkg.MConfig
 
 func init() {
+	pkg.ShowBanner()
 	mconfig = &pkg.MConfig{}
 	mconfig.Namspace = flag.String("namespace", "com.github.mhchlib", "Input Your Namespace")
 	mconfig.RegistryAddress = flag.String("registry_address", "127.0.0.1:2389", "Input Your Registry Address, multiple IP commas separate")
