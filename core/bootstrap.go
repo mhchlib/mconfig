c2efada7dfb2f333da1647ee61f3a4589848c260
huchenhao <775656764@qq.com>
2021-02-22 11:29:23 +0800

    add cron

--- core/bootstrap.go
+++ core/bootstrap.go
@@ -3,6 +3,7 @@ package core
 import (
 	"github.com/mhchlib/mconfig/core/client"
 	"github.com/mhchlib/mconfig/core/config"
+	"github.com/mhchlib/mconfig/core/cron"
 	"github.com/mhchlib/mconfig/core/event"
 	"github.com/mhchlib/mconfig/core/filter"
 	"github.com/mhchlib/mconfig/core/mconfig"
@@ -11,11 +12,12 @@ import (
 
 // InitMconfig ...
 func InitMconfig(mconfig *mconfig.MConfig) func() {
+	event.InitEventBus()
 	config.InitConfigCenter()
 	filter.InitFilterEngine()
 	client.InitClientManagement()
 	store.InitStore(mconfig.StoreType, mconfig.StoreAddress)
-	go event.InitEventBus()
+	cron.InitCron()
 	return EndMconfig()
 }
 
