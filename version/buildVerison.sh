1e860b1be533d8250851ec3783053988d3024820
huchenyang <huchenyang@qianxin.com>
2021-03-21 14:10:39 +0800

    update version

--- version/buildVerison.sh
+++ version/buildVerison.sh
@@ -4,11 +4,7 @@ dist=version/version.go
 
 rm -f $dist
 
-
 echo "package version\n"            >> $dist
 echo "const version = \"$1\"\n"     >> $dist
-echo "func GetVersion() string {"   >> $dist
-echo "	return version"             >> $dist
-echo "}"                            >> $dist
 
 echo "echo project version to version.go success"
--- version/version.go
+++ version/version.go
@@ -1,7 +1,3 @@
 package version
 
-const version = "v1.0.0-79-g07662fe"
-
-func GetVersion() string {
-	return version
-}
+const version = "v1.0.0-80-gb6632f3"
